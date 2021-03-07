d796ec4892bcfa4a98cbcc3d521ffa16418f3c22
Frédéric Le Barzic <fred@lebarzic.fr>
2021-03-07 20:03:31 +0100

    add findByLocation method on GeoNameRepository

--- Repository/GeoNameRepository.php
+++ Repository/GeoNameRepository.php
@@ -18,4 +18,53 @@ class GeoNameRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, GeoName::class);
     }
+
+    public function findByLocation(
+        string $latitude,
+        string $longitude,
+        int $distanceMaximun = 50,
+        int $populationMinimun = 2000,
+        int $limit = 5
+    ): array {
+        $query = $this->createQueryBuilder('g')
+            ->addSelect('f', 'c', 'a1', 'a2', 'a3', 'a4', 't')
+            ->addSelect(
+                '(DEGREES(
+                ACOS(
+                  SIN(RADIANS(:latitude)) * SIN(RADIANS(g.latitude)) + 
+                  COS(RADIANS(:latitude)) * COS(RADIANS(g.latitude)) * 
+                  COS(RADIANS(:longitude - g.longitude))
+                ) 
+              ) * 60 * 1.1515 * 1.609344) AS distance'
+            )
+            ->setParameter(':latitude', $latitude)
+            ->setParameter(':longitude', $longitude)
+            ->leftJoin('g.feature', 'f')
+            ->leftJoin('g.country', 'c')
+            ->leftJoin('g.admin1', 'a1')
+            ->leftJoin('g.admin2', 'a2')
+            ->leftJoin('g.admin3', 'a3')
+            ->leftJoin('g.admin4', 'a4')
+            ->leftJoin('g.timezone', 't');
+
+        if ($distanceMaximun > 0) {
+            $query
+                ->andHaving('distance <= :distance')
+                ->setParameter(':distance', $distanceMaximun);
+        }
+
+        if ($populationMinimun > 0) {
+            $query
+                ->andWhere('g.population >= :population')
+                ->setParameter(':population', $populationMinimun);
+        }
+
+        return $query
+            ->andWhere('g.latitude IS NOT NULL')
+            ->andWhere('g.longitude IS NOT NULL')
+            ->orderBy('distance', 'ASC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
 }
